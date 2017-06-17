7ced04a1c459435eed169a9eacee7e26f854542c
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-06-18 00:28:23 +0700

    update: text processor

--- draft.js
+++
@@ -1,6 +0,0 @@
-console.log("bla, bla, <eatable> something <a b c>".match(/<([^><]+)>/))
-
-let str = "bla, bla, <eatable> something <a b c>"
-let reg = /<([^><]+)>/gi;
-
-console.log(reg.exec(str), reg.exec(str))
