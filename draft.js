ef9a2bbcdfe0c33f396441030d05fbfc126880d4
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-06-18 18:58:23 +0700

    feat: split, group

--- draft.js
+++ draft.js
@@ -1,13 +1,10 @@
 const tokenize = require('./lib/tokenize')
-const debug = require('./lib/fn/debug')
+// const debug = require('./lib/fn/debug')
 
 console.log(
-	tokenize("what the hell is this <thing> bla, bla <yeap> love love")
-		.tag(
-			"tag",
-			/<[^><]+>/,
-			(text, start, end) => debug(text, start, end),
-			(text, start, end) => debug(text, start, end)
-		)
+	tokenize("what the hell is this <thing> bla, bla <yeap>")
+		.tag("tag", /<[^><]+>/)
+		.split("word", /\s+/)
+		.filter(text => text !== "")
 		.tokens
 )
