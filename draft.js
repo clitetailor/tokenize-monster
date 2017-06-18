97f623a434f4298dcdc3df759a1f59ecbbea240f
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-06-18 15:26:14 +0700

    feat: start, end index

--- draft.js
+++ draft.js
@@ -1,9 +1,13 @@
 const tokenize = require('./lib/tokenize')
+const debug = require('./lib/fn/debug')
 
 console.log(
 	tokenize("what the hell is this <thing> bla, bla <yeap> love love")
 		.tag(
+			"tag",
 			/<[^><]+>/,
-			"tag"
+			(text, start, end) => debug(text, start, end),
+			(text, start, end) => debug(text, start, end)
 		)
+		.tokens
 )
